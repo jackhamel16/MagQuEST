15078e5ff799627f322ec6e45bc691833723adcc
Jack Hamel <jackhaml16@gmail.com>
2018-01-18 12:22:10 -0500

    initialized results to be 0 in pulse_interactions.cpp.

--- src/interactions/pulse_interaction.cpp
+++ src/interactions/pulse_interaction.cpp
@@ -13,6 +13,7 @@ const Interaction::ResultArray &PulseInteraction::evaluate(const int time_idx)
   const double time = time_idx * dt;
 
   for(size_t i = 0; i < dots->size(); ++i) {
+    results[i] = Eigen::Vector3d(0, 0, 0);
     for(int n = 0; n < static_cast<int>(pulses->size()); ++n) {
       results[i] += (*pulses)[n]((*dots)[i].position(), time);
     }
