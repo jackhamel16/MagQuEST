f081e301655bd98d1dc1c1335b7f645a6d79a0c0
Jack Hamel <jackhaml16@gmail.com>
2019-09-14 00:28:18 -0400

    Began work on update jfnk_test.  added setup code basically

--- test/JFNK_test.cpp
+++ test/JFNK_test.cpp
@@ -5,8 +5,12 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include "../src/solver/newton.h"
+#include "../src/pulse.h"
 #include "../src/solver/solver.h"
+#include "../src/interactions/pulse_interaction.h"
+
+// PREFACE: This is a terrible example of a unit test.  The JFNK solver relies on so many other
+// modules and they are required to test it, unfortunately.
 
 BOOST_AUTO_TEST_SUITE(JFNK_solver)
 
@@ -18,11 +22,10 @@ struct Universe {
   Universe() : num_solutions(10){};
 
   vec3d ode(vec3d u, vec3d alpha) { 
-    double gamma0 = 175920000000.0;
+    double gamma0 = 221000.0;
     double damping = 0.1;
     const double gamma = gamma0 / (1 + std::pow(damping, 2));
     return -gamma * u.cross(alpha);
-    //return u.cross(alpha);
   }
     
   int factorial(int n) { return (n > 1) ? n * factorial(n - 1) : 1; }
@@ -34,13 +37,11 @@ struct Universe {
 
   vec3d ode_solution(double t, vec3d u0, vec3d alpha)
   {
-    double gamma0 = 175920000000.0;
+    double gamma0 = 221000.0;
     double damping = 0.1;
     const double gamma = gamma0 / (1 + std::pow(damping, 2));
     vec3d sol = u0;
     for(int n = 1; n < 20; ++n) {
-    //vec3d sol = vec3d(0,0,0);
-    //for(int n = 0; n < 25; ++n) {
       sol += std::pow(t * gamma, n) / factorial(n) * recursive_cross(n, alpha, u0);
     }
     return sol;
@@ -58,46 +59,42 @@ BOOST_FIXTURE_TEST_CASE(JFNK_solver, Universe)
 {
   const double tolerance = 1e-3;
 
-  //const double step_size = 0.005;
-  //const int num_of_steps = 30;
-  //const int max_iter = 10;
-  //const vec3d alpha(0.5, 1, 1.5);
   const double step_size = 1e-18;
   const int num_of_steps = 10;
   const int max_iter = 4;
-  const vec3d alpha(0, 1e3, 0);
   std::vector<vec3d> history(num_of_steps);
   std::vector<vec3d> delta_vec(num_of_steps);
   std::vector<vec3d> analytical_history(num_of_steps);
-  //history[0] = vec3d(1, 1, 1);
-  history[0] = vec3d(1e6, 0, 0);
+
+  // Setup a particle
+  const double Ms = 1e6;
+  const vec3d M(Ms, 0, 0);
+  MagneticParticle mp(vec3d(0,0,0), 0.1, 221000, Ms, M);
+  DotVector mp_vec(1);
+  mp_vec[0] = mp;
+  // Create Pulse
+  const double dc_mag = 1e3;
+  const vec3d dc_orientation(0, 1, 0);
+  const vec3d alpha = dc_mag * dc_orientation;
+  Pulse dc_pulse(0, 1, 1, dc_mag, vec3d(1, 0, 0), dc_orientation);
+  PulseVector pulse_vec(1);
+  pulse_vec[0] = dc_pulse;
+  //Set up interactions
+  PulseInteraction pulse_interaction(std::make_shared<DotVector>(mp_vec),
+                                     std::make_shared<PulseVector>(pulse_vec),
+                                     1, step_size);
+
+  history[0] = vec3d(Ms, 0, 0);
   analytical_history[0] = history[0];
+   
 
   //std::cout << std::scientific << std::setprecision(6);
   for(int step = 1; step < num_of_steps; ++step) {
     analytical_history[step] =
         ode_solution(step * step_size, analytical_history[0], alpha);
-    history[step] = history[step - 1] * 0.99;
-    //history[step] = vec3d(0.90, .02, 0.99);
-    delta_vec[step] = history[step] - history[step - 1];
-    // guess here
-    for(int iter = 0; iter < max_iter; ++iter) {
-      std::cout << "Iteration =  " << iter <<"\n";
-      std::cout << "ode= " << ode(history[step], alpha).transpose() << std::endl;
-      std::vector<vec3d> residual_vec(1);
-      residual_vec[0] = history[step-1] - history[step] + step_size * ode(history[step], alpha);
-      std::cout << "Delta before = " << delta_vec[step].transpose() <<"\n";
-      std::cout << "jmv = " << ode_jacobian_matvec(delta_vec[step], alpha)[0].transpose() <<"\n";
-      delta_vec[step] =
-          JFNK_solve(1, step_size, ode_jacobian_matvec(delta_vec[step], alpha),
-                     residual_vec)[0];
-      history[step] = delta_vec[step] + history[step];
-      std::cout << "Residual = " << residual_vec[0].transpose() <<"\n";
-      std::cout << "Delta = " << delta_vec[step].transpose() <<"\n";
-      std::cout << "History = " << history[step].transpose() <<"\n\n";
-    }
-    std::cout << "History norm = " << history[step].norm()<<"\n\n";
   }
+
+
   for(int step = 0; step < num_of_steps; ++step) {
     BOOST_CHECK_MESSAGE(
         (analytical_history[step] - history[step]).norm() / analytical_history[step].norm() < tolerance,
